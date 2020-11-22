31c1059c984b88206a8849f79bcb8a34fe41172d
Ivan Zuboff <anotherdiskmag@gmail.com>
2020-11-22 16:01:52 +0300

    3.1-4

--- 03/code.clj
+++ 03/code.clj
@@ -753,3 +753,54 @@ failed-protagonist-names
 
 (mapset inc [1 1 2 2])
 ; => #{2 3}
+
+; 1. Use the str, vector, list, hash-map, and hash-set functions.
+(str "a" "b" "c" "a")
+; "abca"
+(vector "a" "b" "c" "a")
+; ["a" "b" "c" "a"]
+(list "a" "b" "c" "a")
+; ("a" "b" "c" "a")
+(hash-map :a "b" :c :a)
+; {:c :a, :a "b"}
+(hash-set :a "b" :c :a)
+; #{:c "b" :a}
+
+; 2. Write a function that takes a number and adds 100 to it.
+(defn add100
+  "add 100 to number"
+  [num]
+  (+ num 100))
+
+; (add100 1)
+; 101
+; (add100 1/2)
+; 201/2
+; (add100 0.5)
+; 100.5
+
+; 3. Write a function, dec-maker, that works exactly like the function inc-maker except with subtraction:
+(defn dec-maker
+  "Create a custom decrementor"
+  [dec-by]
+  #(- % dec-by))
+
+(def dec9 (dec-maker 9))
+(dec9 10)
+; => 1
+
+; 4. Write a function, mapset, that works like map except the return value is a set:
+(defn mapset
+  [f coll]
+  "construct set from mapping"
+  (reduce (fn [result part]
+            (conj result (f part)))
+          #{}
+          coll))
+
+(mapset inc [1 1 2 2])
+; => #{3 2}
+
+; 5. Create a function that’s similar to symmetrize-body-parts except that it has to work with weird space aliens with radial symmetry. Instead of two eyes, arms, legs, and so on, they have five.
+
+; 6. Create a function that generalizes symmetrize-body-parts and the function you created in Exercise 5. The new function should take a collection of body parts and the number of matching body parts to add. If you’re completely new to Lisp languages and functional programming, it probably won’t be obvious how to do this. If you get stuck, just move on to the next chapter and revisit the problem later.
